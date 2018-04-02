7c9b8d778d3cce5e73e9072698c701d69d035d9d
Mateusz Holowieszko <holowieszko.m@gmail.com>
2018-04-02 12:54:24 +0200

    error handling / CORS / static

--- app.js
+++ app.js
@@ -12,12 +12,42 @@ const userRoutes = require('./api/routes/users');
 mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds137464.mlab.com:37464/node-rest-library');
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
 
 app.use('/users', userRoutes);
 app.use('/books', bookRoutes);
 app.use('/authors', authorRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
